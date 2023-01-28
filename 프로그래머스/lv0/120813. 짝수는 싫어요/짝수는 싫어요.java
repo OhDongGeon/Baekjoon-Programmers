879d92bc8e831af2fdbcc31476b91e070d04d255
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-01-28 12:19:47 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[n % 2 == 0 ? n / 2 : n / 2 + 1];
+        int cnt = 0;
+        for(int i = 1; i <= n; i += 2) {
+            answer[cnt++] = i;
+        }
+        return answer;
+    }
+}
