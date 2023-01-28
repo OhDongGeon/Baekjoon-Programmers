71dd6f8c1fdb7d11b769980f1d4f6467c69b5cfe
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-01-28 12:24:44 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        if(0 <= hp && hp <=1000) {
+            
+            for(int i = 5; i >= 1; i-=2) {
+                answer += hp / i; 
+                hp = hp % i;
+            }
+        }
+        
+        return answer;
+    }
+}
