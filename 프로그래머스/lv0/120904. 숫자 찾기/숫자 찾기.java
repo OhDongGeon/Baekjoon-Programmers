42e30202abc0490f22a761186f27510e8b217f6d
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-01-29 19:44:05 +0900

    [level 0] Title: 숫자 찾기, Time: 0.14 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+        String[] nums = Integer.toString(num).split("");
+        for(int i = 0; i < nums.length; i++) {
+            if(Integer.parseInt(nums[i]) == k) {
+                answer = i + 1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
