ad4297f258d9ff6fc9599cf62c9c8957757a43cf
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-03 00:42:33 +0900

    [unrated] Title: 둘만의 암호, Time: 0.23 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -4,6 +4,7 @@ class Solution {
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         String[] str_Skip = skip.split("");
         int alhabet_Cnt = 0;
+        StringBuilder sb = new StringBuilder();
         
         for(int i = 0; i < str_Skip.length; i++) {
             alphabet = alphabet.replace(str_Skip[i], "");
@@ -11,10 +12,9 @@ class Solution {
         
         for(int i = 0; i < s.length(); i++) {
             alhabet_Cnt = (alphabet.indexOf(Character.toString(s.charAt(i))) + index) % alphabet.length();
-            answer += alphabet.substring(alhabet_Cnt, alhabet_Cnt + 1);
+            sb.append(alphabet.substring(alhabet_Cnt, alhabet_Cnt + 1));
         }
         
-        
-        return answer;
+        return answer = sb.toString();
     }
 }
