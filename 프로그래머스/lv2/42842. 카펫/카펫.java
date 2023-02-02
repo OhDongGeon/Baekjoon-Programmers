65ce1ae2a0543b973572056e34519d4103d3d2a0
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-02 09:56:32 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        int sum = brown + yellow;
+        
+        
+        for(int i = 1; i * i <= sum; i++) {
+            if(sum % i == 0) {
+                if((sum / i - 2) * (i - 2) == yellow) {
+                    answer[0] = sum / i;
+                    answer[1] = i;
+                }
+            }
+        }          
+        
+        
+        return answer;
+    }
+}
