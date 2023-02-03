7ad63744066f5c634a34e82224df0ed49c36945a
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-03 10:26:24 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.36 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] sides) {
+        int answer = 0;
+
+        Arrays.sort(sides);
+        
+        answer =  (sides[1] + sides[0] - 1) - (sides[1] - sides[0]);   
+            
+        return answer;
+    }
+}
