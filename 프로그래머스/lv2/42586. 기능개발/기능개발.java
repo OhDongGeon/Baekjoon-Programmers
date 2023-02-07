601ae148886498f6cf319f9242370fc8509c7def
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-07 10:58:03 +0900

    [level 2] Title: 기능개발, Time: 0.32 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.java
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -1,24 +1,41 @@
-import java.util.ArrayList;
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer;
+        int[] answer = {};
+        Queue<Integer> q = new LinkedList();
+        ArrayList<Integer> arr = new ArrayList();
+        int cnt = 0;
+        int peek = -1;
 
-        int[] dayOfend = new int[100];
-        int day = -1;
-        for(int i=0; i<progresses.length; i++) {
-            while(progresses[i] + (day*speeds[i]) < 100) {
-                day++;
+        for(int i = 0; i < progresses.length; i++) {
+            q.add((int)Math.ceil((100.0 - progresses[i]) / speeds[i]));
+        }
+
+        while(!q.isEmpty()) {
+            if(peek == -1) {
+                peek = q.peek();
             }
-            dayOfend[day]++;
-        }
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i : dayOfend) {
-            if(i!=0) {list.add(i);}
+
+            if(peek < q.peek()) {
+                arr.add(cnt);
+                peek = -1;
+                cnt = 0;
+            } else if(q.size() == 1) {
+                cnt++;
+                q.poll();
+                arr.add(cnt);
+                cnt = 0;
+            } else {
+                cnt++;
+                q.poll();
+            }
         }
 
-        answer = new int[list.size()];
-        for(int i=0; i<answer.length; i++) {
-            answer[i] = list.get(i);
+        answer = new int[arr.size()];
+
+        for(int item : arr) {
+            answer[cnt++] = item;
         }
 
         return answer;
