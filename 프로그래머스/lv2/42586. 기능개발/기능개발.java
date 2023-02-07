b5cec5e6c3ddc51190a15b125eb486d7f9738348
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-07 10:23:24 +0900

    [level 2] Title: 기능개발, Time: 0.46 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        int cnt = 0;
+        int peek = -1;
+        
+        Queue<Integer> q = new LinkedList();
+        ArrayList<Integer> arr = new ArrayList();
+        
+        for(int i = 0; i < progresses.length; i++) {
+            q.add((int)Math.ceil((100.0 - progresses[i]) / speeds[i]));
+        }
+        
+        while(!q.isEmpty()) {
+            if(peek == -1) {
+                peek = q.peek();
+            }
+
+            if(peek < q.peek()) {
+                arr.add(cnt);
+                peek = -1;
+                cnt = 0;
+            } else if(q.size() == 1) {
+                cnt++;
+                q.poll();
+                arr.add(cnt);
+                cnt = 0;
+            } else {
+                cnt++;
+                q.poll();
+            }
+        }
+        
+        answer = new int[arr.size()];
+        
+        for(int item : arr) {
+            answer[cnt++] = item;
+        }
+        
+        return answer;
+    }
+}
