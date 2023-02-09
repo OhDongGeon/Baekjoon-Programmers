3aeea70eb39659b0ddacafcb6037ebcbb69dbf4a
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-09 13:24:11 +0900

    [level 2] Title: 소수 찾기, Time: 4.91 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -10,11 +10,13 @@ class Solution {
     public void permutetion(String[] number, String[] print, int depth) {
         // print의 길이가 깊이의 길이와 같다면 return 예정
         if(print.length == depth) {
-            // 
+            // 순열한 값을 숫자로 변환 예) 011 -> 11
             int num = Integer.parseInt(String.join("", print));
             int cnt = 0;
             
+            // HashSet안에 순열한 숫자가 있는지 확인
             if(!hs.contains(num)) {
+                // 소수 찾기
                 for(int i = 1; i * i <= num; i++) {
                     if(num % i == 0) {
                         cnt += 2;
@@ -24,20 +26,28 @@ class Solution {
                     }
                 }
                 
+                // 소수가 1과 본인수를 가지고 있으면 소수 
                 if(cnt == 2) {
                     answer++;
                 }
             }
+            // HashSet 추가
             hs.add(num);
             
             return; 
         }
         
+        
         for(int i = 0; i < number.length; i++) {
+            // 방문 했었는지 체크
             if(!visited[i]) {
+                // 방문 체크
                 visited[i] = true;
+                // print자리수에 맞게 number 추가
                 print[depth] = number[i];
+                // 재귀
                 permutetion(number, print, depth + 1);
+                // 재귀 후 방문 체크 해제
                 visited[i] = false;
             }
         }
