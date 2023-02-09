83ae6523b84d5dfbf08496333a433596d56c20a5
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-09 13:03:07 +0900

    [level 2] Title: 소수 찾기, Time: 3.66 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -2,11 +2,15 @@ import java.util.*;
 
 class Solution {
     int answer = 0;
+    // 방문 했었는지 체크하는 변수
     boolean[] visited;
+    // 중복확인을 위한 변수
     HashSet<Integer> hs = new HashSet();
     
     public void permutetion(String[] number, String[] print, int depth) {
+        // print의 길이가 깊이의 길이와 같다면 return 예정
         if(print.length == depth) {
+            // 
             int num = Integer.parseInt(String.join("", print));
             int cnt = 0;
             
@@ -41,11 +45,14 @@ class Solution {
     
     
     public int solution(String numbers) {
+        // numbers를 배열로 변환
         String[] number = numbers.split("");
-        String[] print;
+        // number의 길이로 설정
         visited = new boolean[number.length];
-            
+        
+        // 1자리 수 부터 number의 길이 만큼 재귀
         for(int i = 0; i < number.length; i++) {
+            // new String[i + 1]은 한자리 부터 시작
             permutetion(number, new String[i + 1], 0);
         }
         return answer;
