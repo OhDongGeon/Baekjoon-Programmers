b6f582d63f9256e47b284922d9667f0d7067b0ea
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-10 10:45:27 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 225.43 ms, Memory: 145 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -6,15 +6,20 @@ class Solution {
         int sum = 0;
         HashSet<Integer> hs = new HashSet();
         
+        
+        // elements의 길이 만큼 순회
         for(int i = 0; i < elements.length; i++) {
             sum = 0;
-            
+            // elements의 길이 + i 만큼 순회, 맨끝자리에 도달하여 다시 0번으로 돌아와 더해야 하는 경우가 있음
             for(int j = i; j < elements.length + i; j++) {
+                // elements의 길이를 넘어 갔을때
                 if(elements.length <= j) {
                     sum += elements[j - elements.length];
                 } else {
+                    // elements의 길이 안에 있을때
                     sum += elements[j];
                 }
+                // 중복 제거 
                 hs.add(sum);
             }
         }
