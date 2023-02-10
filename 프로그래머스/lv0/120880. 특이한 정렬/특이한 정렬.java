c76635c07073ec295bc4b413704f901baade1285
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-10 23:02:40 +0900

    [level 0] Title: 특이한 정렬, Time: 0.80 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -3,52 +3,76 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numlist, int n) {
         int[] answer = new int[numlist.length];
-        int answer_idx = 0;
-        int value = Integer.MAX_VALUE;
-        int idx = 0;
-        int pre = 1;
-        int next = 1;
+        int i = 0;
+        ArrayList<Integer> arr = new ArrayList();
         
-        
-        Arrays.sort(numlist);
-        
-        for(int i = 0; i < numlist.length; i++) {
-            if((Math.abs(value - n) > Math.abs(numlist[i] - n))) {
-                value = numlist[i];
-                idx = i;
-            }
+        for(int item : numlist) {
+            arr.add(item);
         }
         
-        answer[0] = numlist[idx];
-        
-        
-        while(answer_idx < numlist.length - 1) {
-            answer_idx++;
-            
-            
-            
-            if(idx - pre < 0) {
-                answer[answer_idx] = numlist[idx + next];
-                next++;
-                continue;
-            } else if(idx + next > numlist.length - 1) {
-                answer[answer_idx] = numlist[idx - pre];
-                pre++;
-                continue;
-            }
-            
-            
-            if(Math.abs(numlist[idx - pre] - n) == (Math.abs(numlist[idx + next] - n))) {
-                answer[answer_idx] = numlist[idx + next];
-                next++;
-            } else if(Math.abs(numlist[idx - pre] - n) > (Math.abs(numlist[idx + next] - n))) {
-                answer[answer_idx] = numlist[idx + next];
-                next++;
+        arr.sort((a, b) -> {
+            int abs_A = Math.abs(a - n);
+            int abs_B = Math.abs(b - n);
+            if (abs_A != abs_B) {
+                return abs_A - abs_B;
             } else {
-                answer[answer_idx] = numlist[idx - pre];
-                pre++;
+                return b - a;
             }
+        });
+        
+        for(int item : arr) {
+            answer[i++] = item;
         }
+        
+        
+        
+        
+//         int answer_idx = 0;
+//         int value = Integer.MAX_VALUE;
+//         int idx = 0;
+//         int pre = 1;
+//         int next = 1;
+        
+        
+//         Arrays.sort(numlist);
+        
+//         for(int i = 0; i < numlist.length; i++) {
+//             if((Math.abs(value - n) > Math.abs(numlist[i] - n))) {
+//                 value = numlist[i];
+//                 idx = i;
+//             }
+//         }
+        
+//         answer[0] = numlist[idx];
+        
+        
+//         while(answer_idx < numlist.length - 1) {
+//             answer_idx++;
+            
+            
+            
+//             if(idx - pre < 0) {
+//                 answer[answer_idx] = numlist[idx + next];
+//                 next++;
+//                 continue;
+//             } else if(idx + next > numlist.length - 1) {
+//                 answer[answer_idx] = numlist[idx - pre];
+//                 pre++;
+//                 continue;
+//             }
+            
+            
+//             if(Math.abs(numlist[idx - pre] - n) == (Math.abs(numlist[idx + next] - n))) {
+//                 answer[answer_idx] = numlist[idx + next];
+//                 next++;
+//             } else if(Math.abs(numlist[idx - pre] - n) > (Math.abs(numlist[idx + next] - n))) {
+//                 answer[answer_idx] = numlist[idx + next];
+//                 next++;
+//             } else {
+//                 answer[answer_idx] = numlist[idx - pre];
+//                 pre++;
+//             }
+//         }
         
         return answer;
     }
