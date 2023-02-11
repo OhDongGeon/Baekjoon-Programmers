34fcdf3e7578cdf51567c37c7a541d4dadced080
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-11 21:20:27 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.13 ms, Memory: 69.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int max_Key = -1;
+        int max = 0;
+        HashMap<Integer, Integer> hm = new HashMap();
+        
+        
+        
+        for(int item : array) {
+            int value = hm.getOrDefault(item, 0) + 1;
+            hm.put(item, value);
+            
+            if(max < value) {
+                max = value;
+                max_Key = item;
+            }
+        }
+        
+        
+        for(Map.Entry<Integer, Integer> item : hm.entrySet()) {
+            if(item.getKey() != max_Key && item.getValue() == max){
+                return -1;
+            }
+        }
+        
+        
+        
+        return answer = max_Key;
+    }
+}
