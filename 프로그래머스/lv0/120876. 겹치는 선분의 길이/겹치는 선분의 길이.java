79b8b65ab9fc87c2b0d9dfc23e27d281eac067bd
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-13 00:01:44 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        int[] arr = new int[200];
+        
+        for(int i = 0; i < lines.length; i++) {
+            for(int j = lines[i][0]; j < lines[i][1]; j++) {
+                arr[j + 100]++;
+            }
+        }
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] >= 2) {
+                answer++;
+            }
+        }
+        
+        
+        return answer;
+    }
+}
