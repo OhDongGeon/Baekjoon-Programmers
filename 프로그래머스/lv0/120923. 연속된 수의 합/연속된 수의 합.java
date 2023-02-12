43ee23d5b0a593c89d2512c85ec33bd1547a2698
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-12 16:02:20 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,6 +1,10 @@
 class Solution {
     public int[] solution(int num, int total) {
         int[] answer = new int[num];
+        // 시작
+        // total / num 은 가운데 숫자
+        // num / 2는 가운데 숫자의 좌측 우측갈 수 있는 최대수
+        // total % num == 0 아닌경우 숫자의 좌측 우측갈 수 있는 최대수 줄어 듦
         int start = total % num == 0 ? (total / num) - (num / 2) : (total / num) - (num / 2) + 1;
         int cnt = 0;
         
@@ -8,8 +12,6 @@ class Solution {
             answer[cnt] = start + cnt; 
             cnt++;
         }
-        
-        
         
         return answer;
     }
