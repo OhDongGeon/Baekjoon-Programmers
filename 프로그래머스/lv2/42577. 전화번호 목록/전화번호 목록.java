616ef05050d3457709a84478b6f8832093485288
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-13 11:58:12 +0900

    [level 2] Title: 전화번호 목록, Time: 292.29 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,9 +4,22 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
+        // 효율성 2개 초과
+        // for(int i = 0; i < phone_book.length; i++) {
+        //     for(int j = 0; j < phone_book.length; j++) {
+        //         if(i != j && (phone_book[i].startsWith(phone_book[j]) || phone_book[j].startsWith(phone_book[i]))) {
+        //             answer = false;
+        //             break;
+        //         }
+        //     }
+        //     if(!answer) {
+        //         break;
+        //     }
+        // }
+        
         Arrays.sort(phone_book);
-
-        for(int i = 0; i < phone_book.length -1; i++) {
+        
+        for(int i = 0; i < phone_book.length - 1; i++) {
             if(phone_book[i + 1].startsWith(phone_book[i])) {
                 answer = false;
                 break;
