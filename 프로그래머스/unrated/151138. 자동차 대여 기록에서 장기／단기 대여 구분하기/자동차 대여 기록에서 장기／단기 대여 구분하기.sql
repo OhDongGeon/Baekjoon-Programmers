16c71da5d7e4f64f24b87d4cdd4fe3ff62262a67
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-14 12:37:47 +0900

    [unrated] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/unrated/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,4 +1,29 @@
 -- 코드를 입력하세요
+  #   SELECT HISTORY_ID
+  #        , CAR_ID
+  #        , DATE_FORMAT(START_DATE, '%Y-%m-%d')  START_DATE
+  #        , DATE_FORMAT(END_DATE, '%Y-%m-%d')    END_DATE
+  #        , (CASE WHEN TIMESTAMPDIFF(DAY, START_DATE, END_DATE) + 1 < 30
+  #                THEN '단기 대여'
+  #                ELSE '장기 대여'
+  #           END)                                RENT_TYPE
+  #     FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+  #    WHERE START_DATE LIKE '2022-09%'
+  # ORDER BY HISTORY_ID DESC
+  
+  #   SELECT HISTORY_ID
+  #        , CAR_ID
+  #        , DATE_FORMAT(START_DATE, '%Y-%m-%d')  START_DATE
+  #        , DATE_FORMAT(END_DATE, '%Y-%m-%d')    END_DATE
+  #        , (CASE WHEN TIMESTAMPDIFF(DAY, START_DATE, END_DATE) + 1 < 30
+  #                THEN '단기 대여'
+  #                ELSE '장기 대여'
+  #           END)                                RENT_TYPE
+  #     FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+  #    WHERE YEAR(START_DATE)     = '2022'
+  #      AND MONTH(START_DATE)    = '09'
+  # ORDER BY HISTORY_ID DESC
+  
     SELECT HISTORY_ID
          , CAR_ID
          , DATE_FORMAT(START_DATE, '%Y-%m-%d')  START_DATE
@@ -8,5 +33,5 @@
                  ELSE '장기 대여'
             END)                                RENT_TYPE
       FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-     WHERE START_DATE LIKE '2022-09%'
+     WHERE DATE_FORMAT(START_DATE, '%Y%m')     = '202209'
   ORDER BY HISTORY_ID DESC
