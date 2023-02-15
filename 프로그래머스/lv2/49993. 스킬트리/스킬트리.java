bb28677d267be0e642a887782969303b9362468a
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-02-15 10:58:22 +0900

    [level 2] Title: 스킬트리, Time: 7.58 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -1,18 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        ArrayList<String> skillTrees = new ArrayList<String>(Arrays.asList(skill_trees));
-        //ArrayList<String> skillTrees = new ArrayList<String>();
-        Iterator<String> it = skillTrees.iterator();
 
-        while (it.hasNext()) {
-            if (skill.indexOf(it.next().replaceAll("[^" + skill + "]", "")) != 0) {
-                it.remove();
+        for(int i = 0; i < skill_trees.length; i++) {
+            String tree = skill_trees[i].replaceAll("[^"+ skill +"]", "");
+
+            if(skill.substring(0, tree.length()).equals(tree)) {
+                answer++;
             }
         }
-        answer = skillTrees.size();
         return answer;
     }
 }
