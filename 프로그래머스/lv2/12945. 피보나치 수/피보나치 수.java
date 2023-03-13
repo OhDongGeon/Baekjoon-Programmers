54e2e5f565396aa8e69e92ad160f06b36b12bb8a
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-03-13 22:24:26 +0900

    [level 2] Title: 피보나치 수, Time: 3.91 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] num = new int[n + 1];
+        num[0] = 0;
+        num[1] = 1;
+        
+        for(int i = 2 ; i < num.length; i++) {
+            num[i] = (num[i - 1] + num[i - 2]) % 1234567;
+        }
+        answer = num[n];
+        return answer;
+    }
+}
