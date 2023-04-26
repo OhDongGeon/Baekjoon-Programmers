9fec12e069059f83be4a1a9396eff0a60b0ce096
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-04-26 12:04:10 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine();
+        System.out.print(id + "??!");
+    }
+}
