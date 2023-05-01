56eb41f8d94aa2ada7ff58577e846ff053eacbf8
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-05-01 16:15:34 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        int sum = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        
+        System.out.print(sum);
+    }
+}
