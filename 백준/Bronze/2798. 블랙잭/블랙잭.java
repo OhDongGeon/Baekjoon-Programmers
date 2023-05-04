0ca29c7b81ee96632b8c619470dbacec4a994273
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-05-04 20:50:30 +0900

    [Bronze II] Title: 블랙잭, Time: 228 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] arr = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) { 
+                for (int k = j + 1; k < N; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (sum <= M) {
+                        max = Math.max(max, sum);
+                    }
+                }
+            }
+        }
+        System.out.print(max);
+    }
+}
