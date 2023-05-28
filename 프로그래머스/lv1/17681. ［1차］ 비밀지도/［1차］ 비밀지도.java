19816226fbf69baee4814796d57cb544e2bfd78f
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-05-28 15:14:13 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,12 +1,13 @@
 class Solution {
     public String map(int num, int length) {
-        String binary = "";
+        StringBuilder binary = new StringBuilder(length);
         
         for (int i = 0; i < length; i++) {
-            binary = num % 2 + binary;
+            binary.insert(0, num % 2);
             num /= 2;
         }
-        return binary;
+        
+        return binary.toString();
     }
     
     
@@ -14,20 +15,58 @@ class Solution {
         String[] answer = new String[n];
         
         for (int i = 0; i < n; i++) {
-            String pwd1 = map(arr1[i], n); 
-            String pwd2 = map(arr2[i], n); 
-            String password = "";
+            StringBuilder password = new StringBuilder(n);
+            String pwd1 = map(arr1[i], n);
+            String pwd2 = map(arr2[i], n);
             
             for (int j = 0; j < n; j++) {
                 if (pwd1.charAt(j) == '1' || pwd2.charAt(j) == '1') {
-                    password += "#";
+                    password.append('#');
                 } else {
-                    password += " ";
+                    password.append(' ');
                 }
-            } 
-            answer[i] = password;
+            }
+            
+            answer[i] = password.toString();
         }
         
         return answer;
     }
 }
+
+
+
+
+// class Solution {
+//     public String map(int num, int length) {
+//         String binary = "";
+        
+//         for (int i = 0; i < length; i++) {
+//             binary = num % 2 + binary;
+//             num /= 2;
+//         }
+//         return binary;
+//     }
+    
+    
+//     public String[] solution(int n, int[] arr1, int[] arr2) {
+//         String[] answer = new String[n];
+        
+//         for (int i = 0; i < n; i++) {
+//             String pwd1 = map(arr1[i], n); 
+//             String pwd2 = map(arr2[i], n); 
+//             String password = "";
+            
+//             for (int j = 0; j < n; j++) {
+//                 if (pwd1.charAt(j) == '1' || pwd2.charAt(j) == '1') {
+//                     password += "#";
+//                 } else {
+//                     password += " ";
+//                 }
+//             } 
+//             answer[i] = password;
+//         }
+        
+//         return answer;
+//     }
+// }
