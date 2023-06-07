97a0d92d48fbd882c740fd7f21a55de780723cea
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-06-07 10:54:45 +0900

    [unrated] Title: A 강조하기, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,15 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        myString = myString.toLowerCase();
-        
-        for (int i = 0; i < myString.length(); i++) {
-            String word = myString.charAt(i) + "";
-            if (word.equals("a")) {
-                word = word.toUpperCase();
-            }
-            answer += word;
-        }
+        String answer = myString.toLowerCase().replace("a", "A");
         return answer;
     }
 }
