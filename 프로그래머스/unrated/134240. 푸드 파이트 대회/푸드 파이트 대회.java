13249dded4c2415f826cbd89f4403b6b322f87eb
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-06-13 11:31:46 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.10 ms, Memory: 85.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder sb = new StringBuilder(); 
+        sb.append(0);
+        
+        for (int i = food.length - 1; i >= 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                sb.insert(0, i);
+                sb.insert(sb.length(), i);
+            }
+        }
+        return sb.toString();
+    }
+}
