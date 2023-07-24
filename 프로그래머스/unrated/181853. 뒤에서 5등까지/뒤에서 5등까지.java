a1865fced04c980c981ed48a30b0a1e8bdea63e0
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-07-24 16:41:50 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.45 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -3,12 +3,11 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[5];
-        int cnt = 0;
-        
+
         Arrays.sort(num_list);
 
         for (int i = 0; i < 5; i++) {
-            answer[cnt++] = num_list[i];
+            answer[i] = num_list[i];
         }
         
         return answer;
