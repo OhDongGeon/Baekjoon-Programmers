a8a6f388c1d2e09076d5f57ed985f87438adb59e
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-07-25 21:24:15 +0900

    [unrated] Title: l로 만들기, Time: 21.75 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,17 +1,6 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder answer = new StringBuilder();
-        
-        for (int i = 0; i < myString.length(); i++) {
-            char word = myString.charAt(i);
-            
-            if (word < 'l')  {
-                answer.append("l");
-            } else {
-                answer.append(word);
-            }
-        }
-        
-        return answer.toString();
+        String answer = myString.replaceAll("[^l-z]", "l");
+        return answer;
     }
 }
