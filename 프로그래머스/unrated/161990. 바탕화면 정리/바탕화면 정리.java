28fc1d4a8d398671ce5064a170f489ccbca10101
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-07-27 22:07:25 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.12 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        List<String> arr = new ArrayList<>();
+        
+        for (String item : wallpaper) {
+            arr.add(item);
+        }
+        
+        int firstRow = Integer.MAX_VALUE;
+        int firstCol = Integer.MAX_VALUE;
+        int lastRow = 0;
+        int lastCol = 0;
+        
+        for (int i = 0; i < wallpaper.length; i++) {
+            if (arr.get(i).contains("#")) {
+                firstRow = Math.min(firstRow, i);
+                firstCol = Math.min(firstCol, arr.get(i).indexOf("#"));
+                lastRow = Math.max(lastRow, i + 1);
+                lastCol = Math.max(lastCol, arr.get(i).lastIndexOf("#") + 1);
+            }
+        }
+        
+        int[] answer = {firstRow, firstCol, lastRow, lastCol};
+        return answer;
+    }
+}
