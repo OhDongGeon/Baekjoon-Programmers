1341c5d21f6dccedc72cc568b464bfe5b8be2a65
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-07-27 17:53:02 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.47 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.java
@@ -15,14 +15,14 @@ class Solution {
                 answer+=map.get(weights[i] * 2.0 / 3.0);
             }
             if (map.containsKey(weights[i] * 3.0 / 4.0)) {
-               answer+=map.get(weights[i] * 3.0 / 4.0);
+                answer+=map.get(weights[i] * 3.0 / 4.0);
             }
             if (map.containsKey(weights[i] / 2.0)) {
                 answer+=map.get(weights[i] / 2.0);
             }
-            
             map.put(weights[i] * 1.0, map.getOrDefault(weights[i] * 1.0, 0) + 1);
         }
+        
         
         return answer;
     }
