680f9b7d991845185216a6755052e3ecc9458e7e
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-08-10 21:02:40 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -3,14 +3,15 @@ class Solution {
         int answer = 0;
         
         for (int i = 0; i < number.length - 2; i++) {
-            for (int j = i + 1; j < number.length - 1; j++) {
-                for (int k = j + 1; k < number.length; k++) {
-                    if(number[i] + number[j] + number[k] == 0) {
-                        answer++;
-                    }
-                }
-            }
+             for (int j = i + 1; j < number.length - 1; j++) {
+                  for (int k = j + 1; k < number.length; k++) {
+                      if (number[i] + number[j] + number[k] == 0) {
+                          answer++;
+                      }
+                  }
+             }
         }
+        
         return answer;
     }
 }
