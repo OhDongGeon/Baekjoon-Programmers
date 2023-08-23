ed48cddc80df4492ee0a6468f99c0e9c2caca4ca
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-08-23 11:47:22 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 220 ms, Memory: 17772 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        
+        long[] dp = new long[n + 1];
+        dp[0] = 0; 
+        dp[1] = 1;
+        
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.print(dp[n]);
+    }
+}
