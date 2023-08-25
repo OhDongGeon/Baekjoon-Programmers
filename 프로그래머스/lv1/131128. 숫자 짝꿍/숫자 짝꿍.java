d9fd8fa2a6089051b4326c0c7d6d7ce4644a4f86
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-08-25 21:22:36 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -1,32 +1,36 @@
+import java.util.*;
+
 class Solution {
     public String solution(String X, String Y) {
-        String answer = "";
-        StringBuilder re = new StringBuilder();
-        int[] cntX = new int[10];
-        int[] cntY = new int[10];
+        int[] xArr = new int[10];
+        int[] yArr = new int[10];
         
-        for(int i = 0; i < X.length(); i++) {
-            cntX[Character.getNumericValue(X.charAt(i))]++;
+        for (int i = 0; i < X.length(); i++) {
+            xArr[X.charAt(i) - '0']++;
         }
         
-        for(int i = 0; i < Y.length(); i++) {
-            cntY[Character.getNumericValue(Y.charAt(i))]++;
+        for (int i = 0; i < Y.length(); i++) {
+            yArr[Y.charAt(i) - '0']++;
         }
         
-        for(int i = 0; i < cntX.length; i++) {
-            if(cntX[i] != 0 && cntY[i] != 0) {
-                for(int j = 0; j < Math.min(cntX[i], cntY[i]); j++) {
-                    re.append(i);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < xArr.length; i++) {
+            if (xArr[i] != 0 && yArr[i] != 0) {
+                for (int j = 0; j < Math.min(xArr[i], yArr[i]); j++) {
+                    sb.append(i);
                 }
-            }
+            } 
         }
         
-        answer = re.reverse().toString();
+        sb.reverse();
         
-        if(re.length() == 0) {
+        String answer = "";
+        if (sb.length() == 0) {
             answer = "-1";
-        } else if(re.substring(0,1).toString().equals("0")) {
+        } else if (sb.substring(0,1).equals("0")) {
             answer = "0";
+        } else {
+            answer = sb.toString();
         }
         
         return answer;
