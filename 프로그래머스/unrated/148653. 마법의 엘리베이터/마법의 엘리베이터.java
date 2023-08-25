fc9d6a7d64595cd4606eabe3e6e58c45104fea3c
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-08-25 23:03:38 +0900

    [unrated] Title: 마법의 엘리베이터, Time: 0.06 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/unrated/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,18 +1,20 @@
 class Solution {
     public int solution(int storey) {
         int answer = 0;
-        
+
         while (storey > 0) {
             int rest = storey % 10;
             storey /= 10;
             
             if (rest == 5) {
+                
                 if (storey % 10 >= 5) {
                     answer += 10 - rest;
                     storey++; 
                 } else {
                    answer += rest; 
                 }
+                
             } else if (rest > 5) {
                 answer += 10 - rest;
                 storey++;
@@ -20,10 +22,7 @@ class Solution {
                 answer += rest;
             }
         }
-        
-        
-        
-        
+
         return answer;
     }
 }
