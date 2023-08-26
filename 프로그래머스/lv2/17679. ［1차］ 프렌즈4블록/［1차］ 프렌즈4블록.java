811f9b15fa8d2a77aa93db28e0e8185c5711c44d
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-08-26 23:00:22 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 1.04 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -37,12 +37,13 @@ class Solution {
                 }
             }
             
+            // 맨 끝에서 부터 순환
             for (int i = m - 1; i >= 0; i--) {
                 for (int j = n - 1; j >= 0; j--) {
-                    
+                    // 공백일 경우 그 라인 위쪽으로 순환 
                     if (block[i][j] == '-') {
                         for (int k = i - 1; k >= 0; k--) {
-                            
+                            // 공백이 아닌경우 위치변경
                             if (block[k][j] != '-') {
                                 block[i][j] = block[k][j];
                                 block[k][j] = '-';
