20f73fb64a54d179787e97b4627557795af53cc6
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-09-07 23:01:51 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,20 +1,6 @@
 class Solution {
-    int answer = -1;
+    int answer = 0;
     boolean[] chk;
-    
-    public void dfs(int k, int[][] dungeons, int cnt) {
-        for(int i = 0; i < dungeons.length; i++) {
-            if(k >= dungeons[i][0] && !chk[i]) {
-                chk[i] = true;
-                
-                dfs(k - dungeons[i][1], dungeons, cnt + 1);
-                
-                chk[i] = false;
-            }
-        }
-        answer = Math.max(answer, cnt);
-    }
-    
     
     public int solution(int k, int[][] dungeons) {
         chk = new boolean[dungeons.length];
@@ -22,5 +8,18 @@ class Solution {
         dfs(k, dungeons, 0);
         
         return answer;
+    }
+    
+    public void dfs(int k, int[][] dungeons, int cnt) {
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            if (dungeons[i][0] <= k && !chk[i]) {
+                chk[i] = true;
+                dfs(k - dungeons[i][1], dungeons, cnt + 1);
+                chk[i] = false;
+            }
+            
+        }
+        answer = Math.max(answer, cnt);
     }
 }
