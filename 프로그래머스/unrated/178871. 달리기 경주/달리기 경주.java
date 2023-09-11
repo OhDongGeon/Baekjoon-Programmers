fd891bfb239569547b2d7516910c1a62e58d7e9a
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-09-11 20:35:15 +0900

    [unrated] Title: 달리기 경주, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
-        HashMap<String, Integer> hm = new HashMap();
         
-        for (int i = 0; i < players.length; i++) {
+        Map<String, Integer> hm = new HashMap<>();
+        
+        for (int i = 0; i < players.length; i ++) {
             hm.put(players[i], i);
         }
         
-        for (String call : callings) {
-            int rank = hm.get(call);
-            String before = players[rank - 1];
+        for (String callName : callings) {
+            int callRank = hm.get(callName);
+            String beforeName = players[callRank - 1];
             
-            players[rank - 1] = call;
-            players[rank] = before;
+            players[callRank] = beforeName;
+            players[callRank - 1] = callName;
             
-            hm.put(call, rank - 1);
-            hm.put(before, rank);
+            hm.put(beforeName, callRank);
+            hm.put(callName, callRank - 1);
         }
         
         return players;
