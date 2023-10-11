81e157154f22e82d837cf7c6b4c936217cce7c2c
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-10-11 23:43:36 +0900

    [level 3] Title: 양과 늑대, Time: 0.19 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -8,8 +8,7 @@ class Solution {
     int maxSheep = 0;
     
     public int solution(int[] info, int[][] edges) {
-        int answer = 0;
-        
+
         visited = new boolean[info.length];
         infoCopy = info;
         edgesCopy = edges;
@@ -32,14 +31,15 @@ class Solution {
         } else {
             wolfCnt++;
         }
-
+        
         // 양이 늑대의 수와 같거나 작으면 잡아 먹힌다!
         if (sheepCnt <= wolfCnt) {
-            visited[nextNode] = false;
+            // 현재 방문하고 있는 노드
             return;
         }
         
         for (int i = 0; i < edgesCopy.length; i++) {
+
             if (visited[edgesCopy[i][0]] && !visited[edgesCopy[i][1]]) {
                 dfs(visited, edgesCopy[i][1], sheepCnt, wolfCnt);
                 visited[edgesCopy[i][1]] = false;
