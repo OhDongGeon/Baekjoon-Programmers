4abb363ee82c29bedf96a4093846c271716533e3
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-10-18 11:00:58 +0900

    [level 3] Title: 양과 늑대, Time: 0.33 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     
     static boolean[] visited;
@@ -8,31 +6,27 @@ class Solution {
     static int maxSheep = 0;
     
     public int solution(int[] info, int[][] edges) {
-
+        
         visited = new boolean[info.length];
         infoCopy = info;
         edgesCopy = edges;
         
-        // dfs (방문할 노드, 양의수, 늑대수)
         dfs(0, 0, 0);
         
         return maxSheep;
     }
     
-    
-    public static void dfs (int nextNode, int sheepCnt, int wolfCnt) {
-        visited[nextNode] = true;
+    public static void dfs(int next, int sheepCnt, int wolfCnt) {
         
-        // 다음 방문할 노트가 양이면 양의 수 증가 아니면 늑대의 수 증가
-        if (infoCopy[nextNode] == 0) {
+        visited[next] = true;
+        
+        if (infoCopy[next] == 0) {
             sheepCnt++;
-            // 최대 양의 수
             maxSheep = Math.max(maxSheep, sheepCnt);
         } else {
             wolfCnt++;
         }
         
-        // 양이 늑대의 수와 같거나 작으면 잡아 먹힌다!
         if (sheepCnt <= wolfCnt) {
             return;
         }
@@ -41,12 +35,8 @@ class Solution {
             if (visited[edgesCopy[i][0]] && !visited[edgesCopy[i][1]]) {
                 dfs(edgesCopy[i][1], sheepCnt, wolfCnt);
                 visited[edgesCopy[i][1]] = false;
-                
-                // 전체 노드가 양일 경우
-                if (maxSheep == infoCopy.length) {
-                    return;
-                }
             }
         }
     }
+    
 }
