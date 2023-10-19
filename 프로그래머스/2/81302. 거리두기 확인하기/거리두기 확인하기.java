b7913b64303e2fad6a972159c8659d2bb365ee87
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-10-19 21:39:05 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.61 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -0,0 +1,78 @@
+class Solution {
+    //               상, 하, 좌, 우
+    static int[] x = {0, 0, -1, 1};
+    static int[] y = {1, -1, 0, 0};
+    static char[][] place;
+    static boolean[][] visited;
+    static int value = 1;
+    static int test = 0;
+
+    public int[] solution(String[][] places) {
+        
+        int[] answer = new int[places.length];
+        
+        for (int i = 0; i < places.length; i++) {
+            
+            place = new char[5][5];
+            visited = new boolean[5][5];
+            test  = i ;
+            for (int j = 0; j < places[i].length; j++) {
+                place[j] = places[i][j].toCharArray();
+            }
+            
+            for (int row = 0; row < 5 ; row++) {
+                for (int col = 0; col < 5; col++) {
+                    if (place[row][col] == 'P') {
+                        dfs(row, col, 0);
+                        if (value == 0) {
+                            break;
+                        }
+                    }
+                }
+                if (value == 0) {
+                    break;
+                }
+            }
+            answer[i] = value;
+            value = 1;
+        }
+        return answer;
+    }
+    
+    
+    public static void dfs(int row, int col, int endCnt) {
+        
+        if (endCnt >= 2) {
+            return;
+        }
+        
+        visited[row][col] = true;
+        System.out.println();
+        for (int i = 0; i < 4; i++) {
+            int moveY = row + y[i]; // 상하
+            int moveX = col + x[i]; // 좌우
+            
+            if (moveY < 0 || moveY >= 5 || moveX < 0 || moveX >= 5 || visited[moveY][moveX]) {
+                continue;
+            }
+            
+            if (place[moveY][moveX] == 'O') {
+                dfs(moveY, moveX, endCnt + 1);
+            } else if (place[moveY][moveX] == 'P') {
+                value = 0;
+                return;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
