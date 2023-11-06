2a11a5fbcbdd9a3583482c2cae37f1594368a6be
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-11-06 22:00:22 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.01 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870#) 
 
 ### 성능 요약
 
-메모리: 74.3 MB, 시간: 0.18 ms
+메모리: 74.4 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 21:59:47
+2023년 11월 1일 22:0:13
 
 ### 문제 설명
 
--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,42 +1,30 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] sequence, int k) {
+        
         int[] answer = new int[2];
-        Queue<Integer> q = new LinkedList<>();
         int sum = 0;
-        int idx = 0;
-        int len = 1000000;
-        for (int i = 0; i < sequence.length; i++) {
-            if (sequence[i] == k) {
-                answer[0] = i;
-                answer[1] = i;
-                break;
+        int first = 0;
+        int second = 0;
+        int length = sequence.length;
+        int pre = Integer.MAX_VALUE;
+        
+        while (second != length) {
+            if (sum > k || first == length) {
+                sum -= sequence[second++];
+            } else {
+                sum += sequence[first++];
             }
-
-            q.add(sequence[i]);
-            sum += sequence[i];
-
-            if (sum == k) {
-                answer[0] = idx;
-                answer[1] = i;
-                len = i - idx;
-            } else if (sum > k) {
-                int tmp = 0;
-                while (sum > k) {
-                    tmp = q.poll();
-                    sum -= tmp;
-                    idx++;
-
-                    if (sum == k && i - idx < len) {
-                        answer[0] = idx;
-                        answer[1] = i;
-                        len = i - idx;
-                        break;
-                    }
+            
+            if (sum == k) {    
+                int minus = first - second;
+                if (pre > minus) {
+                    answer[0] = second;
+                    answer[1] = first - 1;
+                    pre = minus;
                 }
             }
         }
-
+        
         return answer;
     }
 }
