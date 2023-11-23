804fa79e76aa0ebda7c436d7899bda093580d7c5
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-11-23 21:08:14 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 23.97 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -0,0 +1,32 @@
+class Solution
+{
+    public int solution(int [][]board)
+    {
+        int answer = 0;
+        int x = board.length + 1;
+        int y = board[0].length + 1;
+        int[][] plusBoard = new int[x][y];
+        
+        for (int i = 1; i < x; i++) {
+            for (int j = 1; j < y; j++) {
+                 plusBoard[i][j] = board[i - 1][j - 1];
+            }
+        }
+        
+        for (int i = 1; i < x; i++) {
+            for (int j = 1; j < y; j++) {
+                if (plusBoard[i][j] == 1) {
+                    int top = plusBoard[i - 1][j];
+                    int left = plusBoard[i][j - 1];
+                    int topleft = plusBoard[i - 1][j - 1];
+                    
+                    int minValue = Math.min(Math.min(top, left), topleft) + 1;
+                    plusBoard[i][j] = minValue;
+                    answer = Math.max(answer, minValue);
+                }
+            }
+        }
+
+        return answer * answer;
+    }
+}
