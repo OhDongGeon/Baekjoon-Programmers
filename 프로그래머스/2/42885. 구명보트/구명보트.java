1f56a0691db9642b465780e0fa3854db002652f6
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-12-07 20:30:57 +0900

    [level 2] Title: 구명보트, Time: 9.75 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,16 +3,17 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        int chk = 0;
+        int idx = 0;
         
         Arrays.sort(people);
-        
-        for(int i = people.length - 1; i >= 0; i--) {
-            if(i < chk) {
+
+        for (int i = people.length -1; i >= 0; i--) {
+            
+            if (i < idx) {
                 break;
-            } else if(people[i] + people[chk] <= limit) {
+            } else if (people[i] + people[idx] <= limit) {
                 answer++;
-                chk++;
+                idx++;
             } else {
                 answer++;
             }
