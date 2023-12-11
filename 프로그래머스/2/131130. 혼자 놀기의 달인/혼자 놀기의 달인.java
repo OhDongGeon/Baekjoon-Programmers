40feb5d849e8bc9966dcc3f64d7b9bf682d91543
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-12-11 21:05:22 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.41 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.java
@@ -0,0 +1,52 @@
+// 8 6 3 7 2 5 1 4
+// 1     1     1 1
+// 8은 4를 찾았음 상자 종료
+// 8 6 3 7 2 5 1 4
+//   2     2 2
+// 6도 찾았던 숫자
+// 8 6 3 7 2 5 1 4
+// 1 2   1 2 2 1 1
+
+import java.util.*;
+
+class Solution {
+    
+    int[] cardsCopy;
+    boolean[] visited;
+    ArrayList<Integer> arr = new ArrayList<>();
+    
+    public int solution(int[] cards) {
+        
+        int cardLength = cards.length;
+        cardsCopy = cards;
+        visited = new boolean[cardLength];
+        
+        for (int i = 0; i < cardLength; i++) {
+            if (!visited[i]) {
+                open(cards[i] - 1, 0);
+            }
+        }
+        
+        int answer = 0;
+        
+        if (arr.size() != 1) {
+            Collections.sort(arr, Collections.reverseOrder());
+            answer = arr.get(0) * arr.get(1);
+        }
+        
+        return answer;
+    }
+    
+    
+    public void open (int cardLocation, int cnt) {
+        if (visited[cardLocation]) {
+            arr.add(cnt);
+            return;
+        }
+        
+        visited[cardLocation] = true;
+        open(cardsCopy[cardLocation] -1, cnt + 1);
+    }
+}
+
+
