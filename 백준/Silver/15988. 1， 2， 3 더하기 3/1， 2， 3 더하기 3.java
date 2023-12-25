4e76288a0c13df5cd6e22008de6b42476684a889
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2023-12-25 21:02:42 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 160 ms, Memory: 22212 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -1,29 +1,21 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        List<Integer> arr = new ArrayList<>();
-        int T = Integer.parseInt(br.readLine());
-        
-        for (int i = 0; i < T; i++) {
-            arr.add(Integer.parseInt(br.readLine()));
-        }
-        
-        int maxValue = Collections.max(arr);
-        long[] dp = new long[maxValue];
+
+        long[] dp = new long[1_000_000];
         dp[0] = 1;
         dp[1] = 2;
         dp[2] = 4;
-        
+
         for (int i = 3; i < dp.length; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1_000_000_009;
         }
-        
-        for (Integer num : arr) {
-           System.out.println(dp[num - 1]); 
-        } 
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            System.out.println(dp[Integer.parseInt(br.readLine()) - 1]);
+        }
     }
 }
