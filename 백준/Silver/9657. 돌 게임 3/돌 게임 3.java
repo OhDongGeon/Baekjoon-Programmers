b26404e65de45258d4a494d7b28168f702799e3f
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-01-04 21:00:14 +0900

    [Silver III] Title: 돌 게임 3, Time: 132 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        boolean[] dp = new boolean[1001];
+        dp[1] = true;
+        dp[3] = true;
+        dp[4] = true;
+        
+        for (int i = 5; i <= N; i++) {
+            if (dp[i - 1] && dp[i - 3] && dp[i - 4]) {
+                dp[i] = false;
+            } else {
+                dp[i] = true;
+            }
+        }
+        System.out.println(dp[N] ? "SK" : "CY");
+    }
+}
