321e2e88d4a1133fd34184af0b33803323b21c83
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-03-07 21:20:32 +0900

    [Silver I] Title: 삼각 그래프, Time: 536 ms, Memory: 106628 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.java
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.java
@@ -21,10 +21,7 @@ public class Main {
                 map[i][0] = Integer.parseInt(line.split(" ")[0]);
                 map[i][1] = Integer.parseInt(line.split(" ")[1]);
                 map[i][2] = Integer.parseInt(line.split(" ")[2]);
-            }
 
-
-            for (int i = 0; i < N; i++) {
                 if (i == 0) {
                     map[i][2] += map[i][1];
                 } else if (i == 1) {
@@ -37,7 +34,7 @@ public class Main {
                     map[i][2] += Math.min(map[i][1], Math.min(map[i - 1][1], map[i - 1][2]));
                 }
             }
-
+            
             System.out.println(caseNumber + ". " + map[N - 1][1]);
         }
     }
