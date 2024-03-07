0dac0fd4f5fc62e4c51cce3369167e2b0265345f
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-03-07 21:17:52 +0900

    [Silver I] Title: 삼각 그래프, Time: 552 ms, Memory: 106464 KB -BaekjoonHub

---
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.java
@@ -0,0 +1,44 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int caseNumber = 0;
+
+        while (true) {
+
+            int N = Integer.parseInt(br.readLine());
+
+            if (N == 0) {
+                break;
+            }
+
+            caseNumber++;
+            int[][] map = new int[N][3];
+
+            for (int i = 0; i < N; i++) {
+                String line = br.readLine();
+                map[i][0] = Integer.parseInt(line.split(" ")[0]);
+                map[i][1] = Integer.parseInt(line.split(" ")[1]);
+                map[i][2] = Integer.parseInt(line.split(" ")[2]);
+            }
+
+
+            for (int i = 0; i < N; i++) {
+                if (i == 0) {
+                    map[i][2] += map[i][1];
+                } else if (i == 1) {
+                    map[i][0] += map[i - 1][1];
+                    map[i][1] += Math.min(map[i][0], Math.min(map[i - 1][1], map[i - 1][2]));
+                    map[i][2] += Math.min(map[i][1], Math.min(map[i - 1][1], map[i - 1][2]));
+                } else {
+                    map[i][0] += Math.min(map[i - 1][0], map[i - 1][1]);
+                    map[i][1] += Math.min(map[i][0], Math.min(map[i - 1][0], Math.min(map[i - 1][1], map[i - 1][2])));
+                    map[i][2] += Math.min(map[i][1], Math.min(map[i - 1][1], map[i - 1][2]));
+                }
+            }
+
+            System.out.println(caseNumber + ". " + map[N - 1][1]);
+        }
+    }
+}
