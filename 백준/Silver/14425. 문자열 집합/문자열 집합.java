76d83b82fd1e162733bb4f537a86af477ecab71f
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-03-13 16:11:57 +0900

    [Silver IV] Title: 문자열 집합, Time: 408 ms, Memory: 31104 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -8,22 +8,16 @@ public class Main {
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        String[] S = new String[N];
-
-        for (int i = 0; i < N; i++) {
-            S[i] = br.readLine();
-        }
 
         Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < M; i++) {
-            String line = br.readLine();
-            map.put(line, map.getOrDefault(line, 0) + 1);
+        for (int i = 0; i < N; i++) {
+            map.put(br.readLine(), 0);
         }
 
         int answer = 0;
-        for (int i = 0; i < N; i++) {
-            if (map.containsKey(S[i])) {
-                answer += map.get(S[i]);
+        for (int i = 0; i < M; i++) {
+            if (map.containsKey(br.readLine())) {
+                answer++;
             }
         }
         System.out.println(answer);
