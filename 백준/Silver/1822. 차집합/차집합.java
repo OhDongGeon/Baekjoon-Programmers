a8c54bd17f5b0098cfe834b3d006c2da203e6794
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-03-19 11:29:59 +0900

    [Silver IV] Title: 차집합, Time: 1632 ms, Memory: 205112 KB -BaekjoonHub

---
+++ 백준/Silver/1822. 차집합/차집합.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int aLength = Integer.parseInt(st.nextToken());
+        int bLength = Integer.parseInt(st.nextToken());
+
+        Map<Integer, Integer> map = new TreeMap<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < aLength; i++) {
+            int element = Integer.parseInt(st.nextToken());
+            map.put(element, element);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < bLength; i++) {
+            map.remove(Integer.parseInt(st.nextToken()));
+        }
+
+        if (map.size() == 0) {
+            System.out.println(0);
+        } else {
+            System.out.println(map.size());
+            StringBuilder sb = new StringBuilder();
+            for (Integer item : map.keySet()) {
+                sb.append(item).append(" ");
+            }
+            System.out.println(sb);
+        }
+    }
+}
