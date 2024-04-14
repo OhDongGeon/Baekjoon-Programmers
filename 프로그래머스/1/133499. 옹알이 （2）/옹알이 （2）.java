6006b40511314db9d770a9dc77fa5eefe89c4746
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-04-14 14:37:43 +0900

    [level 1] Title: 옹알이 (2), Time: 0.67 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String[] babbling) {
+        
+        String[] overlap = {"ayaaya", "yeye", "woowoo", "mama"};
+        String[] pronunciation = {"aya", "ye", "woo", "ma"};
+        int answer = 0;
+        
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < pronunciation.length; j++) {
+                babbling[i] = babbling[i].replace(overlap[j], "1");
+                babbling[i] = babbling[i].replace(pronunciation[j], " ");
+            }
+        }
+        
+        for (int i = 0; i < babbling.length; i++) {
+            if (babbling[i].trim().length() == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
