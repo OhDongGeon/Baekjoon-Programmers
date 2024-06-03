c05a0dfd27d86518c6844984bb019d9442e849b6
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-06-03 22:27:00 +0900

    [level 0] Title: 글자 지우기, Time: 5.87 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,21 +1,16 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
         
-        char[] my = my_string.toCharArray();
-        boolean[] check = new boolean[my.length];
+        String[] my = my_string.split("");
         
         for (int i = 0; i < indices.length; i++) {
-            check[indices[i]] = true;
+            my[indices[i]] = "";
         }
-        
+
         String answer = "";
         for (int i = 0; i < my.length; i++) {
-            if (check[i]) {
-                continue;
-            }
             answer += my[i];
         }
-        
         return answer;
     }
 }
