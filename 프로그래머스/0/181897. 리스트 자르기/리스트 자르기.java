f6837a2f1c525af3c64dd157313d499c55df531e
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-06-18 22:34:52 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int n, int[] slicer, int[] num_list) {
+        
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        if (n == 1) {
+            for (int i = 0; i <= slicer[1]; i++) {
+                arr.add(num_list[i]);
+            }
+        } else if (n == 2) {
+            for (int i = slicer[0]; i < num_list.length; i++) {
+                arr.add(num_list[i]);
+            }            
+        } else if (n == 3) {
+            for (int i = slicer[0]; i <= slicer[1]; i++) {
+                arr.add(num_list[i]);
+            }           
+        } else {
+            for (int i = slicer[0]; i <= slicer[1]; i += slicer[2]) {
+                arr.add(num_list[i]);
+            }            
+        }
+      
+        return arr;
+    }
+}
