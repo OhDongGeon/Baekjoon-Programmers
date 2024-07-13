a845e52eed5cea516bfa9b612db08dc70b315ec5
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-07-13 21:10:21 +0900

    [level 0] Title: 두 수의 합, Time: 13.60 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,12 +1,32 @@
-import java.math.*;
+// import java.math.*;
 
 class Solution {
     public String solution(String a, String b) {
         
-        BigInteger num1 = new BigInteger(a);
-        BigInteger num2 = new BigInteger(b);
-        String answer = num1.add(num2).toString();
+        // BigInteger num1 = new BigInteger(a);
+        // BigInteger num2 = new BigInteger(b);
+        // String answer = num1.add(num2).toString();
     
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        int alen = a.length() - 1;
+        int blen = b.length() - 1;
+        int over = 0;
+        
+        while (alen >= 0 || blen >= 0 || over > 0) {
+            
+            int sum = over;
+            
+            if (alen >= 0) {
+                sum += a.charAt(alen--) - '0';
+            }
+            if (blen >= 0) {
+                sum += b.charAt(blen--) - '0';
+            }
+            
+            answer.append(sum % 10);
+            over = sum / 10;
+        }
+        
+        return answer.reverse().toString();
     }
 }
