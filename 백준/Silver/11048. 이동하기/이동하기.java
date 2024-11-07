551ca7a666ecf9e17e3acb61ee0e87dabacedd3e
오동건 (OhDongGeon) <121795660+OhDongGeon@users.noreply.github.com>
2024-11-07 13:41:46 +0900

    [Silver II] Title: 이동하기, Time: 416 ms, Memory: 75076 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N + 1][M + 1];
+        
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Math.max(arr[i][j] + arr[i - 1][j], arr[i][j] + arr[i][j - 1]);
+            }
+        }
+        System.out.println(arr[N][M]);
+    }
+}
