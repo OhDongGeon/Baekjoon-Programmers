ede7219267d9cf30f874b5fa6f8d2f7d45ccf0e2
오동건 (OhDongGeon) <121795660+OhDongGeon@users.noreply.github.com>
2024-12-31 12:54:11 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 1.03 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -3,27 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
         
-        int length = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> rank[o1] - rank[o2]);
+        
         for (int i = 0; i < attendance.length; i++) {
             if (attendance[i]) {
-                length++;
+                pq.offer(i);
             }
         }
-        
-        int[][] participate = new int[length][2];
-        int idx = 0;
-        for (int i = 0; i < rank.length; i++) {
-            if (attendance[i]) {
-                participate[idx][0] = rank[i];
-                participate[idx][1] = i;
-                idx++;
-            }
-        }
-        
-        Arrays.sort(participate, (o1, o2) -> {
-            return o1[0] - o2[0];
-        });
-        
-        return 10000 * participate[0][1] + 100 * participate[1][1] + participate[2][1];
+
+        return 10000 * pq.poll() + 100 * pq.poll() + pq.poll();
     }
 }
