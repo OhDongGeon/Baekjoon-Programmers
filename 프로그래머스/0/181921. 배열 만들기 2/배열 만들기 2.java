cc5459a2d25220e43d0cc396fae31cc389264162
오동건 (OhDongGeon) <121795660+OhDongGeon@users.noreply.github.com>
2025-01-01 14:54:48 +0900

    [level 0] Title: 배열 만들기 2, Time: 68.13 ms, Memory: 117 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        List<Integer> list = new ArrayList<>();
+        
+        for (int i = l; i <= r; i++) {
+            String num = String.valueOf(i);
+            boolean chk = true;
+            
+            for (char item : num.toCharArray()) {
+                if (item != '0'&& item != '5') {
+                    chk = false;
+                    break;
+                }
+            }
+            if (chk) {
+                list.add(i);
+            }
+        }
+        
+        return list.size() == 0 ? new int[] {-1} : list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
