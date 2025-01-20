813cf2d9a4c6a0d3c17f9c5cb3d07e324f8d2dae
오동건 (OhDongGeon) <121795660+OhDongGeon@users.noreply.github.com>
2025-01-20 12:14:10 +0900

    [level 2] Title: [1차] 캐시, Time: 35.70 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        LinkedList<String> list = new LinkedList<>();
+        
+        if (cacheSize == 0) {
+            return cities.length * 5;
+        }
+        
+        for (int i = 0; i < cities.length; i++) {
+            String city = cities[i].toUpperCase();
+            
+            if (list.contains(city)) {
+                answer++;
+                list.remove(city);
+            } else {
+                answer += 5;
+                if (list.size() == cacheSize) {
+                    list.pollFirst();
+                }
+            }
+            list.add(city);
+        }
+        return answer;
+    }
+}
