7ec3cb04bd4414a2d99ab3d396ae9b463102bc7f
오동건 (OhDongGeon) <121795660+OhDongGeon@users.noreply.github.com>
2025-03-11 10:05:22 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    static int computer, network, answer;
+    static int[][] net;
+    static boolean[] visited;
+    static Queue<Integer> queue = new LinkedList<>();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        computer = Integer.parseInt(br.readLine());
+        network = Integer.parseInt(br.readLine());
+        net = new int[computer + 1][computer + 1];
+        
+        for (int i = 0; i < network; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int com1 = Integer.parseInt(st.nextToken());
+            int com2 = Integer.parseInt(st.nextToken());
+            
+            net[com1][com2] = net[com2][com1] = 1;
+        }
+        
+        visited = new boolean[computer + 1];
+        answer = 0;
+        bfs();
+        
+        System.out.println(answer == 0 ? 0 : --answer);
+    }
+    
+    static void bfs() {
+        queue.add(1);
+        visited[1] = true;
+        
+        while (!queue.isEmpty()) {
+            int num = queue.poll();
+            answer++;
+            for (int i = 1; i <= computer; i++) {
+                if (!visited[i] && net[num][i] == 1) {
+                    queue.add(i);
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+}
