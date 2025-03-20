fc89fc7a0ec93c30c2a881db4649fbc43e12d267
오동건 (OhDongGeon) <121795660+OhDongGeon@users.noreply.github.com>
2025-03-20 22:08:54 +0900

    [Silver IV] Title: 카드2, Time: 176 ms, Memory: 36956 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> dq = new ArrayDeque<>();
+        
+        for (int i = 1; i <= N; i++) {
+            dq.add(i);
+        }
+        
+        while (dq.size() != 1) {
+            dq.remove();
+            int num = dq.remove();
+            dq.addLast(num);
+        }
+        
+        System.out.println(dq.peek());
+    }
+}
