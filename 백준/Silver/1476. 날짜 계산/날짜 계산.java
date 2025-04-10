259cd1ad90a01cbf110a93ead5eccb623b184c35
오동건 (OhDongGeon) <121795660+OhDongGeon@users.noreply.github.com>
2025-04-10 21:16:54 +0900

    [Silver V] Title: 날짜 계산, Time: 108 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int E = Integer.parseInt(st.nextToken()); 
+        int S = Integer.parseInt(st.nextToken()); 
+        int M = Integer.parseInt(st.nextToken()); 
+        int year = 0;
+        
+        while(true){
+            year++;
+            if((year - E) % 15 == 0 && (year - S) % 28 == 0 && (year - M) % 19 == 0){  
+                break;
+            }
+        }
+        
+        System.out.println(year);
+    }
+}
